5203b95367febf5ad9171e9b3b65bb52277bb9a3
dung812 <knguyn16@gmail.com>
2022-10-22 09:31:46 +0700

    Done add to cart (ajax)

---
+++ Ecommerce-Project/ShoesShop.UI/ShoesShop.DTO/CartViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ShoesShop.Data;
+using ShoesShop.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesShop.DTO
+{
+    public class CartViewModel
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public int ItemId { get; set; }
+        public int ProductId { get; set; }
+        public string NameItem { get; set; }
+        public string ImageItem { get; set; }
+        public int AttributeId { get; set; }
+        public string AttributeName { get; set; }
+        public int UnitPrice { get; set; } //  Giá gốc
+        public int PromotionPercent { get; set; } // % Giảm giá
+        public int CurrentPriceItem { get; set; } // Giá đã trừ % giảm
+        public int Quantity { get; set; }
+        public int TotalDiscountedPrice { get; set; } // Tổng số tiền đã giảm
+
+        public Double TotalPrice // tổng tiền của sản phẩm
+        {
+            get
+            {
+                return Quantity * CurrentPriceItem;
+            }
+        }
+        public CartViewModel()
+        {
+
+        }
+        public CartViewModel(int idProduct, int quantity, int attributeId)
+        {
+            var attribute = context.AttributeValues.FirstOrDefault(model => model.AttributeValueId == attributeId);
+            var item = context.Products.FirstOrDefault(model => model.ProductId == idProduct);
+
+            this.ItemId = 0;
+            this.ProductId = idProduct;
+            this.NameItem = item.ProductName;
+            this.ImageItem = item.Image;
+            this.AttributeId = (int)attribute.AttributeId;
+            this.AttributeName = attribute.Name;
+            this.UnitPrice = int.Parse(item.OriginalPrice.ToString());
+            this.PromotionPercent = item.PromotionPercent;
+            this.CurrentPriceItem = int.Parse(item.OriginalPrice.ToString()) - ((int.Parse(item.OriginalPrice.ToString()) * int.Parse(item.PromotionPercent.ToString())) / 100);
+            this.Quantity = quantity;
+            this.TotalDiscountedPrice = (this.UnitPrice - this.CurrentPriceItem) * quantity;
+        }
+    }
+}
--- Ecommerce-Project/ShoesShop.UI/ShoesShop.UI/Controllers/CartController.cs
+++ Ecommerce-Project/ShoesShop.UI/ShoesShop.UI/Controllers/CartController.cs
@@ -1,24 +1,114 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoesShop.Domain;
+using ShoesShop.DTO;
 
 namespace ShoesShop.UI.Controllers
 {
     public class CartController : Controller
     {
+        public List<CartViewModel> GetCartSession() // Create list cart and save in session 
+        {
+            // Get customer info data of session
+            var cartSession = HttpContext.Session.GetString("Cart");
+            var cartInfoSession = JsonConvert.DeserializeObject<List<CartViewModel>>(cartSession != null ? cartSession : "");
+
+            List<CartViewModel> listCart = cartInfoSession as List<CartViewModel>;
+            if (listCart == null)
+            {
+                // If the list item in the cart empty, it will create a list contains items
+                listCart = new List<CartViewModel>();
+                var parseCart = JsonConvert.SerializeObject(listCart);
+                HttpContext.Session.SetString("Cart", parseCart);
+            }
+            return listCart;
+        }
+        public void UpdateCartSession(List<CartViewModel> cart)
+        {
+            // Get customer info data of session
+            var parseCart = JsonConvert.SerializeObject(cart);
+            HttpContext.Session.SetString("Cart", parseCart);
+        }
+        private int Quanlity() // Lấy tổng số sản phẩm giỏ hàng hiện tại
+        {
+            int amount = 0;
+            List<CartViewModel> listCart = GetCartSession();
+            if (listCart != null)
+            {
+                amount = listCart.Sum(model => model.Quantity);
+            }
+            return amount;
+        }
+        private double TotalPrice() //  Lấy tổng số tiền sản phẩm
+        {
+            double total = 0;
+            List<CartViewModel> listCart = GetCartSession();
+            if (listCart != null)
+            {
+                total = listCart.Sum(model => model.TotalPrice);
+            }
+            return total;
+        }
+        private double TotalDiscount() //  Lấy tổng số tiền đã giảm
+        {
+            double total = 0;
+            List<CartViewModel> listCart = GetCartSession();
+            if (listCart != null)
+            {
+                total = listCart.Sum(model => model.TotalDiscountedPrice);
+            }
+            return total;
+        }
+
+        public JsonResult GetCartList()
+        {
+            List<CartViewModel> listCart = GetCartSession();
+            return Json(new { status = 200, carts = listCart, totalPrice = TotalPrice(), totalDiscount = TotalDiscount() });
+        }
+
+        [HttpPost]
+        public JsonResult AddToCart(int productId, int quantity, int attributeId) //  Add item in cart 
+        {
+            //System.Threading.Thread.Sleep(2000);
+
+            if (quantity <= 0)
+            {
+                return Json(new { status = 500, smg = "Invalid quantity" });
+            }
+
+            List<CartViewModel> listCart = GetCartSession();
+            CartViewModel item = listCart.FirstOrDefault(model => model.ProductId == productId && model.AttributeId == attributeId);
+
+            if (item == null)
+            {
+                item = new CartViewModel(productId, quantity, attributeId);
+                if (listCart.Count() == 0)
+                    item.ItemId = 1;
+                else
+                    item.ItemId = listCart[listCart.Count() - 1].ItemId + 1; // Get id of last item and increase + 1 for new item id
+
+                listCart.Add(item);
+                UpdateCartSession(listCart);
+
+                return Json(new { status = 200, smg = "Successfull add product in your cart" });
+            }
+            else // Trường hợp đã tồn tại product trong cart
+            {
+                item.Quantity = item.Quantity + quantity;
+                UpdateCartSession(listCart);
+                return Json(new { status = 200, smg = "Successfull increase quantity this product!" });
+            }
+        }
+
+
+
+
+
+
+
         public IActionResult CartPage()
         {
-            //// Send email
-            //string content = System.IO.File.ReadAllText(Path.Combine(_hostEnvironment.WebRootPath, "template\\NewOrder.html"));
-
-            //content = content.Replace("{{url}}", "123");
-
-            //content = content.Replace("{{OrderId}}", "TestId");
-            //content = content.Replace("{{OrderDate}}", "TestDate");
-            //content = content.Replace("{{TotalMoney}}", "TestMoney");
-            //content = content.Replace("{{CustomerName}}", "Test Name");
-            //content = content.Replace("{{Phone}}", "Test phone");
-            //content = content.Replace("{{Address}}", "Test Address");
-
-            //Functions.SendMail("ntdung8124@gmail.com", "New Order At Footwear", content);
             return View();
         }        
         public IActionResult Checkout()
